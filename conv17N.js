d7dc5450ca08a7190d29af798ffc2634a845161f
nec0cean <>
2019-06-20 22:54:12 +0900

    日付を取得できるように修正。

--- conv17N.js
+++ conv17N.js
@@ -1,6 +1,7 @@
 function conv17N() {
   let now = new Date();
   console.log(now.getFullYear());
-  let DOB = document.getElementById("calendar");
+  let DOB = document.getElementById("calendar").value;
   console.log(DOB);
+
 }
