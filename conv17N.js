e72f35ca73e4cca7445e643d05ee46edfb20ac7b
nec0cean <>
2019-06-23 23:02:24 +0900

    見た目を修正 コンソールではなく画面に直接結果を表示するように修正

--- conv17N.js
+++ conv17N.js
@@ -4,14 +4,14 @@ function conv17N() {
   //生年月日を取得して表示
   let DOBstring = document.getElementById("calendar").value;
   let DOB = Date.parse(DOBstring);
-  console.log("入力された生年月日:" + DOBstring);
+  showDOB.textContent = "入力された生年月日:" + DOBstring;
 
-  //生まれてから現在までの差分を計算して、日数、17歳とN日、17歳とNか月で表示
+  //生まれてから今日までの差分を計算して、日数、17歳とN日、17歳とNか月で表示
   let diff = now - DOB;
   let diffDate = diff / (1000 * 60 * 60 * 24);
-  console.log("現在まで" + Math.floor(diffDate) + "日");
+  showdiffDate.textContent = "生まれてから今日まで " + Math.floor(diffDate) + " 日です";
   let conv17yNday = Math.floor(diffDate) - (17 * 365);
-  console.log("17歳と" + conv17yNday + "日です");
+  show17yNday.textContent = "17歳と" + conv17yNday + "日です";
   let conv17yNmonth = conv17yNday/30;
-  console.log("17歳と" + Math.floor(conv17yNmonth) +"か月です");
+  show17yNmonth.textContent = "17歳と" + Math.floor(conv17yNmonth) + "か月です";
 }
